71ed3d30501ef4932819bfe8928432967d54eccc
Tanxuecong <xuecong.tan@gmail.com>
2020-11-02 22:48:19 -0800

    add first person player object and ability to switch camera

---
+++ Assets/SwitchCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    public Camera CameraOne;
+    public Camera CameraTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraOne.enabled = true;
+        CameraTwo.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CameraOne.enabled = !CameraOne.enabled;
+            CameraTwo.enabled = !CameraTwo.enabled;
+        }
+    }
+}
