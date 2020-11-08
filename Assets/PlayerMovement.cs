2282e0834ce61d238f9c4b0935ef0e2ab8759789
Tanxuecong <xuecong.tan@gmail.com>
2020-11-07 23:36:09 -0800

    stop player movement when the player camera is disabled

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool CanMove = false;
+    public Camera PlayerCamera;
+
     public CharacterController controller;
 
     public float speed = 12f;
@@ -21,39 +24,46 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerCamera.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C))
+            PlayerCamera.enabled = !PlayerCamera.enabled;
+
+        if (PlayerCamera.enabled)
+            CanMove = true;
+        else
+            CanMove = false;
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if(isGrounded && velocity.y < 0)
-        {
             velocity.y = -2f;
-        }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (CanMove)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            doubleJump = true;
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                doubleJump = true;
+            }
+
+            if (Input.GetButtonDown("Jump") && !isGrounded && doubleJump)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                doubleJump = false;
+            }
         }
-
-        if (Input.GetButtonDown("Jump") && !isGrounded && doubleJump)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            doubleJump = false;
-        }
-
-
 
         velocity.y += gravity * Time.deltaTime;
 
--- Assets/SwitchCamera.cs
+++ Assets/SwitchCamera.cs
@@ -5,22 +5,17 @@ using UnityEngine;
 public class SwitchCamera : MonoBehaviour
 {
     public Camera CameraOne;
-    public Camera CameraTwo;
 
     // Start is called before the first frame update
     void Start()
     {
         CameraOne.enabled = true;
-        CameraTwo.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
-        {
             CameraOne.enabled = !CameraOne.enabled;
-            CameraTwo.enabled = !CameraTwo.enabled;
-        }
     }
 }
