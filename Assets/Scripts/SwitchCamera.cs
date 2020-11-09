4e95186e8bb233ccfc1365a01a2540842625c85d
Tanxuecong <xuecong.tan@gmail.com>
2020-11-09 12:36:51 -0800

    add Lsystem tree and reorganize files

---
+++ Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    public Camera CameraOne;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraOne.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+            CameraOne.enabled = !CameraOne.enabled;
+    }
+}
