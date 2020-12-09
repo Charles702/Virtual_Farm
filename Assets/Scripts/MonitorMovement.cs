745464e1fe682f84132d78eb2292c3fd52963185
Tanxuecong <xuecong.tan@gmail.com>
2020-12-08 16:17:28 -0800

    fix sphere and fp movement bug

--- Assets/Scripts/MonitorMovement.cs
+++ Assets/Scripts/MonitorMovement.cs
@@ -19,6 +19,9 @@ public class MonitorMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C))
+            CameraOne.enabled = !CameraOne.enabled;
+
         if (CameraOne.enabled)
             CanMove = true;
         else
